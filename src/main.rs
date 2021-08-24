e8e457b73f78c9fc144268dcad5c278f1e55a365
Simon Morgan <sjm@sjm.io>
2021-08-24 20:18:52 +0100

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,18 @@
+#[derive(Debug)]
+struct CPU {
+    current_operation: u16,
+    registers: [u8; 2],
+}
+
+fn main() {
+    let mut cpu = CPU {
+        current_operation: 0,
+        registers: [0; 2],
+    };
+
+    cpu.current_operation = 0x8014;
+    cpu.registers[0] = 5;
+    cpu.registers[1] = 10;
+
+    println!("{:?}", cpu);
+}
